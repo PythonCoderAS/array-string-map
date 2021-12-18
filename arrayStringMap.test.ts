35d7a5e3e941fc3532424ab515dbe3c8adefc938
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-18 09:17:10 -0500

    Enhance and run

--- arrayStringMap.test.ts
+++ arrayStringMap.test.ts
@@ -1,17 +1,24 @@
 import { assert } from "chai"
-import ArrayStringMap from "./arrayStringMap"
+import ArrayStringMap from "./arrayStringMap.js"
 
-describe("arrayStringMap", () => {
-    const sampleArray1 = [1, 2]
-    const sampleArray2 = [1, 2]
-    const sampleArray3 = [1, 3]
-    const sampleValue1 = 1
-    const sampleValue2 = 2
-    it("Empty map", () => {
-        const arrayStringMap = new ArrayStringMap();
-        assert(arrayStringMap.size === 0, "Empty map size is 0");
-        assert(arrayStringMap.get(sampleArray1) === undefined, "Empty map get returns undefined");
-        assert(!arrayStringMap.has(sampleArray1), "Empty map has returns false");
-        assert([...arrayStringMap.entries()].length === 0, "Empty map entries returns empty array");
+const sampleArray1 = [1, 2]
+const sampleArray2 = [1, 2]
+const sampleArray3 = [1, 3]
+const sampleValue1 = 1
+const sampleValue2 = 2
+
+describe("Empty map", () => {
+    const arrayStringMap = new ArrayStringMap();
+    it("Empty map size is 0", () => {
+        assert(arrayStringMap.size === 0);
+    })
+    it ("Empty map get returns undefined", () => {
+        assert(arrayStringMap.get(sampleArray1) === undefined);
+    })
+    it ("Empty map has returns false", () => {
+        assert(!arrayStringMap.has(sampleArray1));
+    })
+    it ("Empty map entries returns empty array", () => {
+        assert([...arrayStringMap.entries()].length === 0);
     })
 })
